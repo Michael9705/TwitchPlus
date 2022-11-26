4a69def471b88eebb5d45f2fc9ae356e863b8f02
Michael9705 <yifan.cumecs0709@gmail.com>
2022-11-26 16:21:59 -0500

    My first commit

---
+++ src/main/java/com/example/jupiter/service/TwitchException.java
@@ -0,0 +1,10 @@
+package com.example.jupiter.service;
+
+public class TwitchException extends RuntimeException{
+
+    public TwitchException(String errorMessage) {
+
+        super(errorMessage);
+    }
+
+}
